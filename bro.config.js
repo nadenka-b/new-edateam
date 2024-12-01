9368d3bac758b5d42256f7c015bb1047b6625924
Nadezhda Balabanova <nabalabanova@mail.ru>
2024-12-01 20:08:13 +0300

    first

---
+++ bro.config.js
@@ -0,0 +1,22 @@
+const pkg = require("./package");
+
+module.exports = {
+  apiPath: "stubs/api",
+  webpackConfig: {
+    output: {
+      publicPath: `/static/${pkg.name}/${process.env.VERSION || pkg.version}/`,
+    },
+  },
+  /* use https://admin.bro-js.ru/ to create config, navigations and features */
+  navigations: {
+    "edateam.main": "/edateam",
+  },
+  features: {
+    "edateam": {
+      // add your features here in the format [featureName]: { value: string }
+    },
+  },
+  config: {
+    "edateam.api": "/api",
+  },
+};
